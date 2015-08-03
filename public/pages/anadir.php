e8faf2d872d82d53ec4029d040c0cc9bfe2c0f28
pabloHM <hermidamourelle@gmail.com>
2015-08-03 22:00:04 +0200

    Añadido logout

--- public/pages/anadir.php
+++ public/pages/anadir.php
@@ -21,23 +21,13 @@
 			else{
 		?>
 		<nav class="navbar navbar-default">
-			<div class="container-fluid">
+			<div class="container">
 				<div class="navbar-header">
-  					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-    					<span class="sr-only">Toggle navigation</span>
-    						<span class="icon-bar"></span>
-    						<span class="icon-bar"></span>
-    						<span class="icon-bar"></span>
-  					</button>
   					<a class="navbar-brand" href="#">CoockerRecipe</a>
 				</div>
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-  					<!--<ul class="nav navbar-nav">
-    					<li class="active">
-    						<a href="#">Link <span class="sr-only">(current)</span></a>
-    					</li>
-  					</ul>-->
-  					<span class="navbar-form navbar-right">¡Bienvenido, <span class='nameUser'><?php echo $_SESSION['uName']?></span>!</span>
+				<div class='navbar-user'>
+					¡Bienvenido, <span class='nameUser'><?php echo $_SESSION['uName']?></span>!<br>
+					<a id='logout' href='php/logout.php'>Logout</a>
 				</div>
 			</div>
 		</nav>
